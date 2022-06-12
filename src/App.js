626c42607f578918b89d0c09f0cd3b1027487120
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-12 15:43:12 -0500

    Input component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import useForm from './hooks/useForm'
+import Input from './components/Input'
 
 function App () {
-  const[form, handleChange] = useForm({ name: ''})
+  const[form, handleChange] = useForm({ name: '', lastname: ''})
 
   console.log(form)
   return (
     <form>
-      <input 
-        name="name" 
-        placeholder="Name" 
-        value={form.name} 
-        onChange={handleChange}
-        />
+      <Input label="Name" name="name" value={form.name} onChange={handleChange}/>
+      <Input label="Lastname" name="lastname" value={form.lastname} onChange={handleChange}/>
     </form>
   )
 }
