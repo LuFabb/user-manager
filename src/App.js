0572e884386a34fd8b88b93bc9b67dbc53c9149d
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-12 21:24:01 -0500

    Card component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import useForm from './hooks/useForm'
 import Input from './components/Input'
+import Card from './components/Card'
 
 function App () {
   const[form, handleChange] = useForm({ name: '', lastname: ''})
 
   console.log(form)
   return (
-    <form>
-      <Input label="Name" name="name" value={form.name} onChange={handleChange}/>
-      <Input label="Lastname" name="lastname" value={form.lastname} onChange={handleChange}/>
-    </form>
+    <Card>
+      <form>
+        <Input 
+          label="Name" 
+          name="name" 
+          value={form.name} 
+          onChange={handleChange}
+        />
+        <Input 
+          label="Lastname" 
+          name="lastname" 
+          value={form.lastname} 
+          onChange={handleChange}
+        />
+      </form>
+    </Card>
   )
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css'
+
+const Card = ({children}) => {
+  return(
+    <div className='card'>
+      {children}
+    </div>
+  )
+}
+
+
+export default Card
