010b3615e73dda4b025cc4eac587f3b8628bdb59
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-13 20:38:09 -0500

    Button component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import useForm from './hooks/useForm'
 import Input from './components/Input'
 import Card from './components/Card'
 import Container from './components/Container'
+import Button from './components/Button'
 
 function App () {
-  const[form, handleChange] = useForm({ name: '', lastname: ''})
+  const[form, handleChange] = useForm({
+     name: '', 
+     email: '',
+     lastname: ''
+    })
 
   console.log(form)
   return (
@@ -24,6 +29,13 @@ function App () {
           value={form.lastname} 
           onChange={handleChange}
         />
+        <Input 
+          label="Email" 
+          name="email" 
+          value={form.email} 
+          onChange={handleChange}
+        />
+        <Button>Send</Button>
       </form>
       </div>
     </Card>
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import './Button.css'
+
+
+const Button = ({children}) => {
+  return(
+    <button className='button'>
+      {children}
+    </button>
+  )
+}
+
+export default Button
